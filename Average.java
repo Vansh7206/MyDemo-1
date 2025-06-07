7049cdeca718c3e9135e7ebe0e2ede49e3d0a228
Vansh7206 <vansh.chandan@somaiya.edu>
2025-06-07 11:45:50 +0530

    Average of 5 Numbers

---
+++ Average.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Average{
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Enter the 5 Numbers");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+        int e=scanner.nextInt();
+
+        int total=a+b+c+d+e;
+        System.out.println("The total of these numbers is: "+total);
+
+        int average= total/5;
+        System.out.println("Average is: "+average);
+    }
+
+}
