80a3baa2dc0a905021b8acf88f8326df6db85200
Manasi2408 <manas.bhutra@somaiya.edu>
2025-06-08 19:59:16 +0530

    Usecase of Printf

---
+++ Printf.java
@@ -0,0 +1,18 @@
+public class Printf{
+//    This is printf intro program
+    public static void main(String[] args){
+        String name="Vansh";
+        int age=19;
+        char first='V';
+        float height=180;
+        boolean isEmployed=true;
+
+        System.out.printf("Your name is %s\n",name);
+        System.out.printf("Your age is %d\n",age);
+        System.out.printf("Your first letter is %c\n",first);
+        System.out.printf("Your height is %f\n",height);
+        System.out.printf("You are employed %b\n",isEmployed);
+
+
+    }
+}
