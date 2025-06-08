45f6f4c54eece1b8759860c2dfe7f3f37b3b3b2d
Manasi2408 <manas.bhutra@somaiya.edu>
2025-06-08 12:07:50 +0530

    Augmented Method

---
+++ Augment.java
@@ -0,0 +1,29 @@
+public class Augment{
+    // This is arugmented Example
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        a+=b;
+        System.out.println(a);
+
+        int c=10;
+        int d=5;
+        c-=d;
+        System.out.println(c);
+
+        int e=10;
+        int f=5;
+        e*=f;
+        System.out.println(e);
+
+        int g=10;
+        int h=5;
+        g/=h;
+        System.out.println(g);
+
+        int i=10;
+        int j=5;
+        i%=j;
+        System.out.println(i);
+    }
+}
