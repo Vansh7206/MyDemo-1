49daa85812850ec1112fe05c9682c22e82c45839
Vansh7206 <vansh.chandan@somaiya.edu>
2025-06-12 12:07:55 +0530

    Count Occurances Updated

--- Count.java
+++ Count.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-public class Count{
+public class Count {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number");
-        int a = scanner.nextInt();
+        System.out.println("Enter the Number");
+        int num = scanner.nextInt();
+
         int count = 0;
-
-        while(a > 0){
-            int rem = a % 10;
-
-            if(rem == 5){
-                count++;
-            }
-            a = a / 10;
-        }
+         while(num > 0){
+             int rem = num % 10;
+             if(rem == 8){
+                 count++;
+             }
+             num = num / 10;
+         }
         System.out.println(count);
     }
 }
